c411f6e71da6c9b54275ddea5961be514fa0beaf
Jan Geffert <jangeffert@college.harvard.edu>
2017-11-11 16:22:29 -0500

    Comment parts of agentPlayer.py

--- agentPlayers.py
+++ agentPlayers.py
@@ -1,14 +1,21 @@
 import numpy as np
 
-MININT = -100000000
-
 class Agent():
+"""
+	A general agent that plays a 2048 game.
+	Note that this agent is supposed to be to be subclassed.
+"""
 
 	def __init__(self):
+		"""Initialize an agent"""
 		self.scores = []
 		self.maxTiles = []
 
 	def move(self, board):
+		"""
+			Return the agents move (LEFT, RIGHT, UP, DOWN)
+			given a certain board state.
+		"""
 		pass
 
 	def addScore(self, score):
@@ -19,16 +26,24 @@ class Agent():
 
 
 class RandomAgent(Agent):
+"""
+	A random agent which chooses any of the valid moves with equal probability.
+"""
 
 	def __init__(self):
 		self.scores = []
 		self.maxTiles = []
 
 	def move(self, board):
-		move = np.random.choice(list(board.findValidMoves()))
-		return move
+		"""Return a any of the valid moves with equal probability"""
+		return np.random.choice(list(board.findValidMoves()))
+
 
 class HeuristicAgent(Agent):
+"""
+	A greedy agent which chooses a moved based on maximizing a specified
+	heuristic function.
+"""
 
 	def __init__(self, fn="MaxTile"):
 		self.scores = []
@@ -54,7 +69,7 @@ class HeuristicAgent(Agent):
 			return max([self.findValue(child, ply=ply - 1) for child in children], 0)
 
 	def move(self, state):
-		bestVal = MININT
+		bestVal = -sys.maxsize
 		bestAction = None
 		for action in list(state.findValidMoves()):
 			val = self.findValue(self.findChild(action, state))
@@ -95,7 +110,7 @@ class ExpectimaxAgent(Agent):
 		return val
 
 	def move(self, state):
-		bestVal = MININT
+		bestVal = -sys.maxsize
 		bestAction = None
 		for action in list(state.findValidMoves()):
 			val = self.findValue(self.findChild(action, state), self.depth)
