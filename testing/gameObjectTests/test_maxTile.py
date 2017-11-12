15ac4b528138a71644197057756daf0272e882c0
EverettSussman <Everett@MacBook-Air-3.local>
2017-11-11 20:16:03 -0500

    Fix maxTile

--- testing/gameObjectTests/test_maxTile.py
+++ testing/gameObjectTests/test_maxTile.py
@@ -2,12 +2,12 @@ from allGameObjectTests import *
 
 def test_1_maxTile():
 	boardTest = Board(config=config1)
-	assert boardTest.maxTile == config1MaxTile
+	assert boardTest.maxTile() == config1MaxTile
 
 def test_10_maxTile():
 	boardTest = Board(config=config10)
-	assert boardTest.maxTile == config10MaxTile
+	assert boardTest.maxTile() == config10MaxTile
 
 def test_11_maxTile():
 	boardTest = Board(config=config11)
-	assert boardTest.maxTile == config11MaxTile
+	assert boardTest.maxTile() == config11MaxTile
