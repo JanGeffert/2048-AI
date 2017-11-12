9991ec5b2cf091ba9c8556d7c984d48273b48412
EverettSussman <Everett@MacBook-Air-3.local>
2017-11-11 22:31:19 -0500

    Fix getSuccessors test

--- testing/gameObjectTests/test_getSuccessors.py
+++ testing/gameObjectTests/test_getSuccessors.py
@@ -2,7 +2,7 @@ from allGameObjectTests import *
 
 def test_1_getSuccessorsLeft():
 	boardTest = Board(config=config12)
-	successorStates, successorProbs = boardTest.getSuccessors("LEFT")
+	successorStates, successorProbs = boardTest.getAllSuccessors("LEFT")
 	successorGrids = [x.grid for x in successorStates]
 	testGrids, testProbs = config12SuccessorsLeft
 	testProbs = [pytest.approx(x, .01) for x in testProbs]
