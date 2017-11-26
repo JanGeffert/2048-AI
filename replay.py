c8fe98e2eb45f3e779afe3a7cabb692a4f2e26a8
Jan Geffert <jangeffert@college.harvard.edu>
2017-11-25 19:10:08 -0800

    Improve replay command line behavior (#46)
    * Add .gitignore
    
    * Fix replay commandline behavior, add speed flag

--- replay.py
+++ replay.py
@@ -1,14 +1,19 @@
 import os
 import sys
 import argparse
+import subprocess
 
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(description='Welcome to 2048, AI edition.')
 
-	parser.add_argument("log", metavar="LOG")
- 
-	args = parser.parse_args()
+	parser.add_argument("log", metavar="LOG", help="The path to a csv logfile.")
+	parser.add_argument("-s", "--speed", default=500, type=int, help="artifical\
+ delay between turns in milliseconds")
 
-	os.system("python -m http.server")
-	os.system("open http://0.0.0.0:8000/replay/?log=/{}".format(args["log"]))
+	args = parser.parse_args()
+	url = "http://0.0.0.0:8000/replay/?log={}&speed={}".format(args.log, args.speed)
+	with open(os.devnull, 'w') as devnull:
+		subprocess.Popen(["python","-m","http.server"], stdout=devnull, stderr=devnull)
+		subprocess.Popen(["python","-m","webbrowser","-t",url], stdout=devnull, stderr=devnull)
+	sys.exit(0)
