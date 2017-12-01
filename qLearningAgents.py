a7076f7418a6e8e740bc091a66151c3f2b98a7e3
Aaron Conrad Sachs <aaronconradsachs@Carlas-MacBook-Air.local>
2017-12-01 17:02:00 -0500

    implement qlearning prelim

--- qLearningAgents.py
+++ qLearningAgents.py
@@ -2,9 +2,11 @@ import numpy as np
 import sys
 from agents import *
 from collections import Counter
+from evaluators import Evaluator
 
 class qLearningAgent(Agent):
 	""" Abstract class for Q-Learning Agent """
+
 
 	def __init__(self, startState, alpha=1.0, epsilon=0.05, 
 				 gamma=0.8, iters=1000):
@@ -23,83 +25,23 @@ class qLearningAgent(Agent):
 		self.iters = iters
 		self.startState = startState
 
-		self.qValues = Counter()
+		# (eval_function : weight)
+		self.weights = Counter()
 		super().__init__()
-
-		# OFFLINE LEARNING
-		state = self.startState
-		for i in self.iters:
-			# Get move and nextState
-			move = self.findBestMove(state)
-			nextState = state.getSuccessor(move)
-
-			# Check if game is over
-			if state.isGameOver():
-				reward = 0
-				self.update(state, move, nextState, reward)
-				break
-			else:
-				reward = self.getReward(state)
-				self.update(state, move, nextState, reward)
-
-				# Prepare for next iteration
-				state = nextState
-			
-			# Rudimentary progress report
-			if i == self.iters / 10:
-				print("10% Complete Training")
-			elif i == self.iters / 2:
-				print("50% Complete Training")
-
-	def update(self, state, move, nextState, reward):
-		"""
-		Here is where the q-value iteration takes place.
-		"""
-		# Store best next action
-		bestNextAction = self.findBestMove(nextState)
-		# Store best next q-Value
-		bestQVal = self.qValues[(nextState, bestNextAction)]
-
-		# Update current q-Value
-		self.qValues[(state, move)] = \
-			(1 - self.alpha) * self.qValues[(state, move)] + \
-			self.alpha * (reward + self.gamma * bestQVal)
-
-	def getReward(self, state):
-		"""
-		Calculate the reward for being in a certain state.
-		"""
-		pass
-
-	def move(self, state):
-		"""
-		Return the agents move (LEFT, RIGHT, UP, DOWN)
-		given a certain board state.
-		"""
-
-		bestMove = None
-		
-		# Check if no valid moves
-		if state.isGameOver():
-			return bestMove
-
-		# Store valid moves
-		moves = state.validMoves()
-		
-		# Flip coin to determine if random move
-		if np.random.random() < self.epsilon:
-			bestMove = np.random.choice(moves)
-		else:
-			bestMove = self.findBestMove(state)
-
-		return bestMove
 
 	def getQValue(self, state, move):
 		"""
-		Returns the q-Value for a given state-move pair.
+		Returns the q-Value for a given state-move pair by executing our
+		approximating Q(s, a) function
 		"""
+		# COPY BOARD
+		# EXECUTE ACTION, 
+		# GET new_state
 
-		return self.qValues[(state, move)]
+		# Takes into account current weights as well
+		return Evaluator.evaluate(new_state)
+
+		# return self.qValues[(state, move)]
 
 	def findBestMove(self, state):
 		"""
@@ -125,4 +67,48 @@ class qLearningAgent(Agent):
 				bestMove = move
 
 		return bestMove
+
+	def move(self, state):
+		"""
+		Return the agents move (LEFT, RIGHT, UP, DOWN)
+		given a certain board state.
+		"""
+
+		bestMove = None
+		
+		# Check if no valid moves
+		if state.isGameOver():
+			return bestMove
+
+		# Store valid moves
+		moves = state.validMoves()
+		
+		# Flip coin to determine if random move
+		if np.random.random() < self.epsilon:
+			bestMove = np.random.choice(moves)
+		else:
+			bestMove = self.findBestMove(state)
+
+		return bestMove
+
+	def getReward(self, state):
+		"""
+		Calculate the reward for being in a certain state.
+		"""
+		return state.score
+
+	def update_weights(state):
+		move = self.move(state)
+		q = self.getQValue(state, move)
+		# COPY BOARD TO EXECUTE move
+		new_state = EXECUTE(state, move)
+
+        # Get best move (findBestMove maximizes Q value)
+        r = self.getReward(new_state)
+        action_prime = self.findBestMove(state)
+
+        difference = r + self.gamma * self.getQValue(new_state, action_prime) - q
+
+        for eval_func in self.weights.keys():
+        	self.weights[eval_func] = self.weights[eval_func] + self.alpha * difference * Evaluator().method(eval_func)(state, move)
 
